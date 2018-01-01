bb4ff90e6f43688915cf5f9fb1c1cd6ae99eae56
Abhishek jain <abj676@gmail.com>
2018-01-02 02:07:07 +0530

    Initial commit

---
+++ app/src/main/java/c/a171/MyService.java
@@ -0,0 +1,34 @@
+package c.a171;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by abhi on 2/1/18.
+ */
+
+public class MyService extends Service {
+
+    IBinder mBinder = new LocalBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    public class LocalBinder extends Binder {
+        public MyService getServerInstance() {
+            return MyService.this;
+        }
+    }
+
+    public String getTime() {
+        SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return mDateFormat.format(new Date());
+    }
+}
